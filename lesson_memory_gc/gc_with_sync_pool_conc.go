3ae1f82860fbfadb86c511176aa3afe51081e1ef
Igor <SandQuattro@gmail.com>
2024-09-24 21:17:43 +0300

    added sync pool and concurrent use to gc example

---
+++ lesson_memory_gc/gc_with_sync_pool_conc.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"runtime"
+	"runtime/debug"
+	"runtime/trace"
+	"sync"
+)
+
+type MyConcPoolStruct struct {
+	data [1 << 20]byte // 1 MB данных
+}
+
+// go tool trace trace.out
+func main() {
+	// отслеживаем изменение в куче
+	debug.SetGCPercent(-1)
+	//debug.SetGCPercent(10)
+	//debug.SetGCPercent(100)
+	//debug.SetGCPercent(1000)
+
+	// Запись в trace файл
+	f, _ := os.Create("trace.out")
+	trace.Start(f)
+	defer trace.Stop()
+
+	// смотрим в рантайме, сколько программа использует памяти
+	var memStat runtime.MemStats
+	runtime.ReadMemStats(&memStat)
+
+	pool := &sync.Pool{
+		New: func() interface{} {
+			return new(MyConcPoolStruct)
+		},
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(100)
+	for i := 0; i < 100; i++ {
+		go func() {
+			defer wg.Done()
+			s := pool.Get().(*MyConcPoolStruct) // new(MyStruct)
+			for j := 0; j < len(s.data); j++ {
+				s.data[j] = byte(i)
+			}
+
+			fmt.Printf("Allocated new %d MB struct\n", i+1)
+
+			pool.Put(s)
+		}()
+
+		// if i == 50 {
+		// 	// Вызов сборщика мусора вручную
+		// 	runtime.GC()
+		// 	fmt.Println("Garbage collector invoked")
+		// }
+	}
+
+	wg.Wait()
+
+	// смотрим в рантайме, сколько программа использует памяти
+	var memStatNow runtime.MemStats
+	runtime.ReadMemStats(&memStatNow)
+
+	memConsumed := float64(memStatNow.Sys-memStat.Sys) / 1024 / 1024
+	runtime.GC()
+	fmt.Printf("Memory consumed %f MB\n", memConsumed)
+
+}
